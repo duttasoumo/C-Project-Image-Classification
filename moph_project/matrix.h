83fcc18eccb8c899fec76857a57a43451edb8047
Soumodeep Dutta <duttasoumo@gmail.com>
2021-05-22 01:33:56 +0530

    first commit

---
+++ moph_project/matrix.h
@@ -0,0 +1,56 @@
+/*
+ * matrix.h
+ */
+
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+#include <iostream>
+
+class Matrix 
+{
+    public:
+        Matrix(int, int);
+        Matrix();
+        ~Matrix();
+        Matrix(const Matrix&);
+        Matrix& operator=(const Matrix&);
+
+        inline double& operator()(int x, int y) { return p[x][y]; }
+		inline int get_rows_() { return rows_; }
+		inline int get_cols_() { return cols_; }
+
+        Matrix& operator+=(const Matrix&);
+        Matrix& operator-=(const Matrix&);
+        Matrix& operator*=(const Matrix&);
+        Matrix& operator*=(double);
+        Matrix& operator/=(double);
+        Matrix  operator^(int);
+        
+        friend std::ostream& operator<<(std::ostream&, const Matrix&);
+        friend std::istream& operator>>(std::istream&, Matrix&);
+
+        void swapRows(int, int);
+        Matrix transpose();
+
+        static Matrix createIdentity(int);
+
+        // functions on vectors
+        static double dotProduct(Matrix, Matrix);
+
+private:
+        int rows_, cols_;
+        double **p;
+
+        void allocSpace();
+        Matrix expHelper(const Matrix&, int);
+};
+
+Matrix operator+(const Matrix&, const Matrix&);
+Matrix operator-(const Matrix&, const Matrix&);
+Matrix operator*(const Matrix&, const Matrix&);
+Matrix operator*(const Matrix&, double);
+Matrix operator*(double, const Matrix&);
+Matrix operator/(const Matrix&, double);
+
+#endif
